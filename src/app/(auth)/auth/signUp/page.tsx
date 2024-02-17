051d8d50074d3ecaac8cf469fd083a34db708828
Bagas Mahda Dhani <muksinsaja12@gmail.com>
2024-02-17 16:51:47 +0700

    add: slicing Organize Layout for auth page

---
+++ src/app/(auth)/auth/signUp/page.tsx
@@ -0,0 +1,13 @@
+import { FC } from "react";
+
+interface SignUpPageProps {}
+
+const SignUpPage: FC<SignUpPageProps> = ({}) => {
+  return (
+    <div>
+      <h1>SignUpPage</h1>
+    </div>
+  );
+};
+
+export default SignUpPage;
